073234b09166f5f8d42f51c9176e88da8974b393
Sergio Berdeja <sergioberdeja@gmail.com>
2022-07-20 16:02:36 -0600

    shortened using .count properly

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,6 +16,6 @@ class String
 
   def count_sentences
     #binding.pry
-    self.split(/[?!.]/).filter{|sentence| sentence != ''}.count
+    self.split(/[?!.]/).count{|sentence| sentence != ''}
   end
 end
